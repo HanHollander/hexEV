d25d0525cde9e9bb0fa6b92a2c644bd720e0c4c7
Han <han_hollander@hotmail.nl>
2016-10-31 10:15:04 +0100

    simple frame with panel with buttone

--- src/GUI/PuzzleScreen.java
+++ src/GUI/PuzzleScreen.java
@@ -10,6 +10,19 @@ import java.awt.*;
 public class PuzzleScreen {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        JFrame puzzleFrame = new JFrame("Nonogram Puzzle Solver");
+        setPuzzleFrameAttr(puzzleFrame, 1024, 768);
+        Panel panel = new Panel();
+        puzzleFrame.getContentPane().add(panel);
+        JButton b = new JButton("OK");
+        panel.add(b);
+    }
+
+    public static void setPuzzleFrameAttr(JFrame fr, int w, int h) {
+        fr.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        fr.pack();
+        fr.setVisible(true);
+        fr.setLocationRelativeTo(null);
+        fr.setSize(w, h);
     }
 }
