e1a56550b8ad7c792ddbed243f3ebbf9b47ebec2
Han <han_hollander@hotmail.nl>
2016-10-31 09:59:14 +0100

    test

--- src/GUI/PuzzleScreen.java
+++ src/GUI/PuzzleScreen.java
@@ -3,5 +3,13 @@ package GUI;
 /**
  * Created by han on 31-10-16.
  */
+
+import javax.swing.*;
+import java.awt.*;
+
 public class PuzzleScreen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
